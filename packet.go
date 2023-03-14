353a5d36f5d2a13bbb6d226d1337551952a5c1c4
Alireza Saadatnia <alirezasn3@gmail.com>
2023-03-14 15:01:42 +0330

    added Buffer to avoid new allocation for each encoding

--- packet.go
+++ packet.go
@@ -2,19 +2,20 @@ package main
 
 type Packet struct {
 	Payload []byte // max length : 1024*8 - 1 - 1
-	ID      byte   // max length : 1
-	Flags   byte   // max length : 1
+	ID      byte   // length : 1
+	Flags   byte   // length : 1
+	Buffer  []byte // to avoid allocation for each packet
 }
 
 func (p *Packet) EncodePacket() []byte {
 	if len(p.Payload) > 8190 { // 8192 - 1 - 1
 		panic("payload was larger than 8190 bytes")
 	}
-	var bytes []byte
-	bytes = append(bytes, p.Flags)
-	bytes = append(bytes, p.ID)
-	bytes = append(bytes, p.Payload...)
-	return bytes
+	p.Buffer = []byte{}
+	p.Buffer = append(p.Buffer, p.Flags)
+	p.Buffer = append(p.Buffer, p.ID)
+	p.Buffer = append(p.Buffer, p.Payload...)
+	return p.Buffer
 }
 
 func (p *Packet) DecodePacket(bytes []byte) {
