c40afd43b4a25a9884af939c0a9c410c54aae261
Alireza Saadatnia <alirezasn3@gmail.com>
2023-04-06 02:34:38 +0330

    fixed bug

--- scripts/fix-openvpn-client-template.sh
+++ scripts/fix-openvpn-client-template.sh
@@ -2,4 +2,5 @@
 
 PUBLIC_IP=$(curl https://ipinfo.io/ip)
 
-sudo echo >> /etc/openvpn/client-template.txt "route $PUBLIC_IP 255.255.255.255 net_gateway\n fast-io"
+sudo echo >> /etc/openvpn/client-template.txt "route $PUBLIC_IP 255.255.255.255 net_gateway"
+sudo echo >> /etc/openvpn/client-template.txt "fast-io"
